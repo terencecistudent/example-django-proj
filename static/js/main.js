a09923c8c27880dc6aac11d432ec1e63358ca5ea
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-18 22:49:51 +0000

    Commented JS code deleted and styles added to style.css

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,5 @@
 $( document ).ready(function() {
-    $(".nav-link").on("click", function(){
-        $(".nav-link.active").removeClass("active");
-        $(this).addClass("active");
-    });
-
-    // Alert message will disappear after 5 seconds.
+    // Alert message will disappear after 3 seconds.
     setTimeout(function () {
         if ($(".alert").is(":visible")) {
             $(".alert").fadeOut("fast");
