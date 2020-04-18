44ac93f347fa66b1271241e3931e08b8ed0bb237
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-18 22:46:41 +0000

    Dropdown list filters added

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,21 @@
 from django.conf.urls import url
-from .views import all_of_the_books, book_info
+from .views import all_of_the_books, book_info, comics, food, horror
+from .views import mystery, sport, price_asc, price_desc, published_date_asc
+from .views import published_date_desc
 
 
 urlpatterns = [
     url(r'^$', all_of_the_books, name="books"),
     url(r'^(?P<pk>\d+)/$', book_info, name="book_info"),
+    url(r'^comics$', comics, name="comics"),
+    url(r'^food$', food, name="food"),
+    url(r'^horror$', horror, name="horror"),
+    url(r'^mystery$', mystery, name="mystery"),
+    url(r'^sport$', sport, name="sport"),
+    url(r'^price_asc$', price_asc, name="price_asc"),
+    url(r'^price_desc$', price_desc, name="price_desc"),
+    url(r'^published_date_asc$', published_date_asc,
+        name="published_date_asc"),
+    url(r'^published_date_desc$', published_date_desc,
+        name="published_date_desc"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Book
 
 
@@ -8,6 +9,14 @@ def all_of_the_books(request):
     the books to the books.html page
     """
     books = Book.objects.all()
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
     return render(request, "books.html",
                   {"books": books})
 
@@ -15,8 +24,134 @@ def all_of_the_books(request):
 def book_info(request, pk):
     """
     A view that returns information
-    of a single book
+    for a single book
     """
     book = get_object_or_404(Book, pk=pk)
     return render(request, "book_detail.html",
                   {"book": book})
+
+
+def comics(request):
+    books = Book.objects.filter(genre="Comics")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def food(request):
+    books = Book.objects.filter(genre="Food")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def horror(request):
+    books = Book.objects.filter(genre="Horror")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def mystery(request):
+    books = Book.objects.filter(genre="Mystery")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def sport(request):
+    books = Book.objects.filter(genre="Sport")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def price_asc(request):
+    books = Book.objects.all().order_by("price")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def price_desc(request):
+    books = Book.objects.all().order_by("-price")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def published_date_asc(request):
+    books = Book.objects.all().order_by("published_date")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
+
+
+def published_date_desc(request):
+    books = Book.objects.all().order_by("-published_date")
+    paginator = Paginator(books, 12)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+    return render(request, "books.html",
+                  {"books": books})
