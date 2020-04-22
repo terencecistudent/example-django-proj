6729071a4e694bdfd0496b9d3b4e72769da77b93
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-22 18:35:49 +0000

    Add allowed host for Heroku

--- booksonline/settings.py
+++ booksonline/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'teds-books-online.herokuapp.com']
 
 
 # Application definition
