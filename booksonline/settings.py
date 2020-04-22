470e4b1c8f52caa4e623e4b2284209309ba951df
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-22 20:13:43 +0000

    If no search results found then leave message for user, email_host_user/password added to settings.py

--- booksonline/settings.py
+++ booksonline/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -182,4 +182,6 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # Email Messages
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
