222ada17c8366b9b091389e9f2618c7249b67c6a
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-23 22:35:03 +0000

    Changes made to url_reset and reset templates

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -4,10 +4,10 @@ from django.contrib.auth.views import password_reset, password_reset_complete, p
 
 
 urlpatterns = [
-    url('^$',
+    url(r'^$',
         password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
     url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
         {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    url('^complete/$', password_reset_complete, name='password_reset_complete')
+    url(r'^complete/$', password_reset_complete, name='password_reset_complete')
 ]
