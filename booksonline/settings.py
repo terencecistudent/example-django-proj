09b96990b856dd28070c16673404025209dd838b
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-24 02:37:40 +0000

    Email settings changed in settings.py

--- booksonline/settings.py
+++ booksonline/settings.py
@@ -123,7 +123,7 @@ AUTHENTICATION_BACKENDS = [
     'accounts.backends.EmailAuthentication',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Internationalization
@@ -183,6 +183,8 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Email Messages
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_SSL = False 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
